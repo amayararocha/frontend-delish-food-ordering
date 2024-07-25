2ec79e10b6b653168a306847f4b590e13d073d04
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-07-25 17:22:20 -0300

    task: implementando RestaurantCard

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Home.css"
 import MultiItemCarousel from './MultiItemCarousel'
+import RestaurantCard from '../restaurant/RestaurantCard'
 
+const restaurant=[1,1,1,1,1,1,1,1]
 const Home = () => {
   return (
-    <div className=''>
+    <div className='pb-10'>
         <section className='banner -z-50 relative flex flex-col justify-center items-center'>
             <div className='w-[50vw] z-10 text-center'>
                 <p className='text-2xl lg:text-6xl font-bold z-10 py-5'>
@@ -24,6 +26,16 @@ const Home = () => {
         <section className='p-10 lg:py-10 lg:px-20'>
             <p className='text-2xl font-semibold text-gray-400 py-3 pb-10'>Top Meals</p>
            <MultiItemCarousel/> 
+        </section>
+        <section className='px-5 lg:px-20 pt-10'>
+            <h1 className='text-2xl font-semibold text-gray-400 pb-5'>
+                Order From Our Handpicked Favorites
+            </h1>
+            <div className='flex flex-wrap items-center justify-around gap-5'>
+                {
+                    restaurant.map((item)=><RestaurantCard/>)
+                }
+            </div>
         </section>
     </div>
   )
---
+++ src/component/restaurant/RestaurantCard.jsx
@@ -0,0 +1,40 @@
+import { Card, Chip, IconButton } from '@mui/material'
+import React from 'react'
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+function RestaurantCard() {
+  const isOpen = true;
+
+  return (
+    <Card className='w-[18rem]'>
+      <div className={`${isOpen ? 'cursor-pointer' : 'cursor-not-allowed'} relative`}>
+        <img className='w-full h-[10rem] rounded-t-md object-cover'
+          src="https://images.pexels.com/photos/3887985/pexels-photo-3887985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <Chip
+          size='small'
+          className="absolute top-2 left-2"
+          color={isOpen ? "success" : "error"}
+          label={isOpen ? "Open" : "Closed"}
+        />
+      </div>
+      <div className='p-4 textPart lg:flex w-full justify-between'>
+        <div className='space-y-1'>
+          <p className='font-semibold text-lg'>
+            Indian Fast Food
+          </p>
+          <p className='text-gray-500 text-sm'>
+            descrição aqui
+          </p>
+        </div>
+        <div>
+          <IconButton>
+            {isOpen ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          </IconButton>
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+export default RestaurantCard
