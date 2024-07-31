0406c8969dd40bf637ca5d7de8dea457303e3121
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-07-31 17:21:45 -0300

    task: criando customerroute

---
+++ src/component/routes/CustomerRoute.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './component/navbar/Navbar.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../home/Home.jsx';
+import RestaurantDetails from '../restaurant/RestaurantDetails.jsx';
+import Cart from '../cart/Cart.jsx';
+import Profile from '../profile/Profile';
+
+const CustomerRoute = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/account/:register' element={<Home/>}/>
+        <Route path='/restaurant/:city/:title/:id' element={<RestaurantDetails/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/my-profile/*' element={<Profile/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default CustomerRoute
