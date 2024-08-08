602b517a658768ffafda135c2636b2d61bcd3927
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-08-08 16:46:42 -0300

    task: implementado o componente registerform

--- src/component/auth/RegisterForm.jsx
+++ src/component/auth/RegisterForm.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Field, Form, Formik } from "formik";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const initialValues = {
+  fullName: "",
+  email: "",
+  password: "",
+  role: "ROLE_CUSTOMER",
+};
 
 function RegisterForm() {
+  const navigate = useNavigate();
+  const handleSubmit = (values) => {
+    console.log("form values", values)
+  };
   return (
-    <div>RegisterForm</div>
-  )
+    <div>
+      <Typography variant="h5" className="text-center">
+        Register
+      </Typography>
+      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+        <Form>
+          <Field
+            as={TextField}
+            name="fullName"
+            label="Full Name"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+          <Field
+            as={TextField}
+            name="email"
+            label="Email"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+          <Field
+            as={TextField}
+            name="password"
+            label="Password"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            type="password"
+          />
+          <FormControl fullWidth margin="normal">
+            <InputLabel>Role</InputLabel>
+            <Field
+              as={Select}
+              label=""
+              name="role"
+            >
+              <MenuItem value={"ROLE_CUSTOMER"}>Customer</MenuItem>
+              <MenuItem value={"ROLE_RESTAURANT_OWNER"}>Restaurant Owner</MenuItem>
+            </Field>
+          </FormControl>
+          <Button
+            sx={{ mt: 2, padding: "1rem" }}
+            fullWidth
+            type="submit"
+            variant="contained"
+          >
+            Register
+          </Button>
+        </Form>
+      </Formik>
+      <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+        <p>Have an account already?</p>
+        <Button
+          sx={{ ml: 1 }}
+          size="small"
+          onClick={() => navigate("/account/login")}
+        >
+          Login
+        </Button>
+      </Typography>
+    </div>
+  );
 }
 
-export default RegisterForm
+export default RegisterForm;
